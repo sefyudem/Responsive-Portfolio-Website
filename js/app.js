3fac6248c6fb3238669014d727f24e721096f537
Youssef <ydemghart@gmail.com>
2022-04-04 18:59:47 +0000

    first commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+/* --------------- Grab elements from DOM --------------- */
+
+/* --------------- Sticky Navbar --------------- */
+
+/* --------------- Reveal Animation --------------- */
+
+/* --------------- Skills Progress Bar Animation --------------- */
+
+/* --------------- Services Counter Animation --------------- */
+
+/* --------------- Portfolio Filter Animation --------------- */
+
+/* --------------- Modal Pop Up Animation Animation --------------- */
+
+/* --------------- Modal Pop Up Animation Animation --------------- */
+
+/* --------------- Change Active Link On Scroll --------------- */
+
+/* --------------- Change Page Theme --------------- */
+
+/* --------------- Open & Close Navbar Menu --------------- */
